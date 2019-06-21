31835c5785b748d0ee841682f6f3138f8d8adf76
LeMinh167127 <52070859+LeMinh167127@users.noreply.github.com>
2019-06-21 21:53:12 +0700

    project

---
+++ WebFlower/WebFlower/Models/Connection.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace _14130046_DoManhHung.Models
+{
+    public class Connection
+    {
+        public static MySqlConnection conn =
+            new MySqlConnection("Database=test;Data Source=127.0.0.1;User Id=root;Password=");
+        public static bool isOpen()
+        {
+            return conn.State == ConnectionState.Open;
+        }
+        public static void openConnect()
+        {
+            conn.Open();
+        }
+        public static void closeConnect()
+        {
+            conn.Close();
+        }
+    }
+}
---
+++ WebFlower/WebFlower/Models/Member.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFlower.Models
+{
+    public class Member
+    {
+        public String memberID { get; set; }
+
+        public String memberName { get; set; }
+
+        public String email { get; set; }
+
+        public String password { get; set; }
+
+        public String gender { get; set; }
+
+        public int memberRole { get; set; }
+    }
+}
